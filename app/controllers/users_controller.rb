e42268cc14070c6700d2741388f17f15a9242955
macroshin <sckm0315@gmail.com>
2021-08-21 14:45:49 +0000

    [Add] CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,27 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
+    @users = User.find(params[:id])
   end
 
   def edit
+    @users = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
